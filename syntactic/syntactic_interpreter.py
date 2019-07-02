98993636b97429e2e8d8d2912ab4af43c2cbe390
Japonics <japonics.email@gmail.com>
2019-07-03 00:15:28 +0200

    Improved parser - added checking parenthesis etc

--- syntactic/syntactic_interpreter.py
+++ syntactic/syntactic_interpreter.py
@@ -14,31 +14,46 @@ class SyntacticInterpreter:
             print("Error {0}".format(str(e)))
 
     def is_w(self, tokens: List[TokenWord], first: bool = False):
-        # W = W + S
-        # W = W - S
-        # W = S
+
+        # Check if parenthesis are matched
+        opened_parenthesis = 0
+        for token in tokens:
+            if token.type == TokenWordTypes.PARENTHESIS and token.token == '(':
+                opened_parenthesis += 1
+            if token.type == TokenWordTypes.PARENTHESIS and token.token == ')':
+                opened_parenthesis -= 1
+
+        if opened_parenthesis != 0:
+            raise Exception("Wrong tokens (parenthesis not matched): {}".format(self.join_tokens(tokens)))
 
         try:
+            # Check if tokens are closed in parenthesis
+            if self.is_closed_in_parenthesis(tokens) and self.is_c(tokens):
+                if first:
+                    print("W = (W)")
+                    print("W => {}".format(self.join_tokens(tokens)))
+                return True
+
             close_parenthesis_index = self.find_token_and_get_index(")", TokenWordTypes.PARENTHESIS, tokens)
             if close_parenthesis_index is None:
                 close_parenthesis_index = 0
 
             index = self.find_token_and_get_index("+", TokenWordTypes.OPERATOR, tokens, close_parenthesis_index)
             if index is not None:
-                if self.is_w(tokens[0:index]) and self.is_s(tokens[:index:-1]):
+                if self.is_w(tokens[0:index]) and self.is_s(tokens[index + 1::]):
                     if first:
                         print("W = W + S")
                         print("W => {}".format(self.join_tokens(tokens[0:index:])))
-                        print("S => {}".format(self.join_tokens(tokens[:index:-1])))
+                        print("S => {}".format(self.join_tokens(tokens[index + 1::])))
                     return True
 
             index = self.find_token_and_get_index("-", TokenWordTypes.OPERATOR, tokens, close_parenthesis_index)
             if index is not None:
-                if self.is_w(tokens[0:index:]) and self.is_s(tokens[:index:-1]):
+                if self.is_w(tokens[0:index]) and self.is_s(tokens[index + 1::]):
                     if first:
                         print("W = W - S")
                         print("W => {}".format(self.join_tokens(tokens[0:index])))
-                        print("S => {}".format(self.join_tokens(tokens[:index:-1])))
+                        print("S => {}".format(self.join_tokens(tokens[index + 1::])))
                     return True
 
             if self.is_s(tokens):
@@ -52,46 +67,44 @@ class SyntacticInterpreter:
         raise Exception("Wrong tokens: {}".format(self.join_tokens(tokens)))
 
     def is_s(self, tokens: List[TokenWord]):
-        # S = S * C
-        # S = S / C
-        # S = C
-
         try:
+
+            if self.is_closed_in_parenthesis(tokens):
+                return self.is_c(tokens)
+
             close_parenthesis_index = self.find_token_and_get_index(")", TokenWordTypes.PARENTHESIS, tokens)
             if close_parenthesis_index is None:
                 close_parenthesis_index = 0
 
             index = self.find_token_and_get_index("*", TokenWordTypes.OPERATOR, tokens, close_parenthesis_index)
             if index is not None:
-                if self.is_s(tokens[0:index]) and self.is_c(tokens[:index:]):
+                if self.is_s(tokens[0:index]) and self.is_c(tokens[index + 1::]):
                     return True
-                elif self.is_c(tokens[0:index]) and self.is_s(tokens[:index:]):
+                elif self.is_c(tokens[0:index]) and self.is_s(tokens[index + 1::]):
                     return True
 
             index = self.find_token_and_get_index("/", TokenWordTypes.OPERATOR, tokens, close_parenthesis_index)
             if index is not None:
-                if self.is_s(tokens[0:index]) and self.is_c(tokens[:index:-1]):
+                if self.is_s(tokens[0:index:]) and self.is_c(tokens[index + 1::]):
                     return True
-                elif self.is_c(tokens[0:index]) and self.is_s(tokens[:index:]):
+                elif self.is_c(tokens[0:index]) and self.is_s(tokens[index + 1::]):
                     return True
 
             if self.is_c(tokens):
                 return True
+
         except IndexError:
             raise Exception("Wrong tokens: {}".format(self.join_tokens(tokens)))
 
         raise Exception("Wrong tokens: {}".format(self.join_tokens(tokens)))
 
     def is_c(self, tokens: List[TokenWord]):
-        # C = liczba
-        # C = identyfikator
-        # C = (W)
+        if len(tokens) == 0:
+            raise Exception("Wrong tokens (empty tokens): {}".format(self.join_tokens(tokens)))
 
         if len(tokens) == 1:
             c_token = tokens[0]
             if c_token.type == TokenWordTypes.VARIABLE:
-                return True
-            if c_token.type == TokenWordTypes.OPERATOR:
                 return True
             if c_token.type == TokenWordTypes.NUMBER:
                 return True
@@ -100,10 +113,15 @@ class SyntacticInterpreter:
             if c_token.type == TokenWordTypes.INTEGER:
                 return True
 
-        if tokens[0].token == "(" and tokens[-1].token == ")" and self.is_w(tokens[1::-2]):
+        if self.is_closed_in_parenthesis(tokens) and self.is_w(tokens[1:-1:]):
             return True
 
         raise Exception("Wrong tokens: {}".format(self.join_tokens(tokens)))
+
+    def is_closed_in_parenthesis(self, tokens: List[TokenWord]) -> bool:
+        if tokens[0].token == "(" and tokens[-1].token == ")":
+            return True
+        return False
 
     def find_token_and_get_index(self, token_value: str, token_type: TokenWordTypes, tokens: List[TokenWord], from_pos: int = 0):
         for index in range(from_pos, len(tokens)):
